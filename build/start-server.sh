9fb21bc4acde1de467a97450702f16b4b0392e13
helio-frota <00hf11@gmail.com>
2016-06-01 13:45:02 -0300

    CI again

--- build/start-server.sh
+++ build/start-server.sh
@@ -25,12 +25,12 @@ if [ ! -f "wildfly-8.2.1.Final.zip" ]
 then 
   wget http://download.jboss.org/wildfly/8.2.1.Final/wildfly-8.2.1.Final.zip
 fi
-unzip wildfly-8.2.1.Final.zip -qq
+unzip -qq wildfly-8.2.1.Final.zip
 if [ ! -f "kie-server-distribution-6.4.0.Final.zip" ]
 then 
   wget http://download.jboss.org/drools/release/6.4.0.Final/kie-server-distribution-6.4.0.Final.zip
 fi
-unzip kie-server-distribution-6.4.0.Final.zip -qq
+unzip -qq kie-server-distribution-6.4.0.Final.zip
 cp kie-server-6.4.0.Final-ee7.war wildfly-8.2.1.Final/standalone/deployments/
 ./wildfly-8.2.1.Final/bin/add-user.sh -a -u kieserver -p kieserver1! -g admin,kie-server
 ./wildfly-8.2.1.Final/bin/standalone.sh  -c standalone-full.xml -Dorg.kie.server.id=bgold-kie-server -Djava.net.preferIPv4Stack=true > kie.log 2>&1 &
